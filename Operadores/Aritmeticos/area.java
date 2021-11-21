3b6617c5bc644b54ee861fb583ad01ef60ba46e9
Bianca Alves <78497908+Bianca-ht@users.noreply.github.com>
2021-11-21 15:48:50 -0300

    Update area.java

--- Operadores/Aritmeticos/area.java
+++ Operadores/Aritmeticos/area.java
@@ -1,1 +1,40 @@
+/*
+ Operadores aritméticos são utilizados para calculos matematicos.
 
+Multiplicação *  |  a*b
+Divisão /        |  a/b
+Adição +         |  a+b
+Subtração -      |  a-b
+
+EXEMPLO:
+--------> Calcule o valor da area:
+ */
+
+/**
+ *
+ * @author Bianca
+ */
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main (String[] args){
+    Scanner sc = new Scanner (System.in); 
+    
+    double b,B,h, area;
+    b = 6.0;
+    B = 8.0;
+    h = 5.0;
+    
+    area=(b+B)/2.0*h;
+    
+    System.out.println("Area "+ area);
+     
+        
+        
+    }
+}
+
+        /* run:
+          Area 35.0
+        */
