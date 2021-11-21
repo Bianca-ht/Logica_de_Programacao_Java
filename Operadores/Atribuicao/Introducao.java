6324256c8ac54cac5bb07d4845b6a1807954fb26
Bianca Alves <78497908+Bianca-ht@users.noreply.github.com>
2021-11-21 16:10:18 -0300

    Update Introducao.java

--- Operadores/Atribuicao/Introducao.java
+++ Operadores/Atribuicao/Introducao.java
@@ -1,1 +1,25 @@
+    /*
+    Operador de Atribuição, retorna o valor da expressão especificado 
+    pela váriavel após a atribuição.
+
+    Sinal de atribuição ( = ).
+
+
+    EXEMPLO:
+
+        variável = expressão;
+
+        ____________________
+
+        area=(b+B)/2.0*h;
+        _________________
+
+        soma=num1+num2;
+
+     */
+
+    /**
+     *
+     * @author Bianca
+     */
 
