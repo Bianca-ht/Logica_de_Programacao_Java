858219775e904e876bc9b26e658f890577747f2c
Bianca Alves <78497908+Bianca-ht@users.noreply.github.com>
2021-11-22 17:13:11 -0300

    Update math.java

--- funcoes_matematica/math.java
+++ funcoes_matematica/math.java
@@ -13,7 +13,6 @@
         A = Math.sqrt(x);
         B = Math.sqrt(y);
         C = Math.sqrt(25.0);
-        
         System.out.println("Raiz quadrada de " + x + " = " + A);
         System.out.println("Raiz quadrada de " + y + " = " + B);
         System.out.println("Raiz quadrada de 25 = " + C);
@@ -22,7 +21,6 @@
         A = Math.pow(x, y);
         B = Math.pow(x, 2.0);
         C = Math.pow(5.0, 2.0);
-        
         System.out.println(x + " elevado a " + y + " = " + A);
         System.out.println(x + " elevado ao quadrado = " + B);
         System.out.println("5 elevado ao quadrado = " + C);
