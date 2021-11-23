82d3014781b177605dda31f2f7502e8b65eeef3a
Bianca Alves <78497908+Bianca-ht@users.noreply.github.com>
2021-11-23 19:11:47 -0300

    Update systemout.java

--- Metodos/systemout.java
+++ Metodos/systemout.java
@@ -1,1 +1,38 @@
+/**
+ *
+ * @author Bianca
+ */
+-------> Métodos:
+
+   - System.out: representa a saída padrão, permitindo exibir dados no 
+                 console quando executamos uma aplicação.
+     
+   - println() : gera uma string de texto, cria uma nova linha abaixo da atual.
+     
+   - print() gera uma string de texto.
+     
+   - printf(): mostra os dados na saída formatados.
+     
+                      - Especificadores: 
+
+                              - %s    String de caracteres
+                              - %d	Número inteiro decimal 
+                              - %u	Número inteiro decimal sem sinal
+                              - %o	Número inteiro octal sem sinal
+                              - %x, %X	Número inteiro hexadecimal sem sinal, minúsculo ou maiúsculo
+                              - %f	Float
+                              - %2f	Double
+                              - %e, %E	Número real, em notação científica, minúsculo ou maiúsculo
+                              - %b	Boolean
+                              - %c	Caractere (char)
+
+                      - Caracteres:
+
+                              - \t	Tabulação
+                              - \b	Backspace
+                              - \n	Nova Linha
+                              - \r	Retorno de carro
+                              - \’	Aspa simples
+                              - \”	Aspa dupla
+                              - \\	Barra invertida        
 
